d6e4451093e2e13970715688232cf589f26b70ee
akashongit <akashgeevarghese.mec@gmail.com>
2018-03-03 17:33:23 +0530

    for cloning

--- listener.js
+++ listener.js
@@ -3,7 +3,7 @@ console.log("Content script loaded!!");
 document.onload = context();
 function context(){
   // console.log("Adding context");
-  context = prompt("Enter Context");
+  context = prompt("Enter Context Please don't cancel");
   console.log("New context added!!  "+context);
   var sending = browser.runtime.sendMessage({url : window.location.href, context:context});
 
